80cc934cad9f9df4f55f8efa83bdcf792c61a323
Alexander Mangel <cygnusfear@gmail.com>
2023-11-30 15:16:53 -0500

    chore: use prompts

--- gitadd.js
+++ gitadd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 
 import { spawn } from "bun";
-import inquirer from "inquirer";
+import prompts from "prompts";
 import kleur from "kleur";
 
 async function runGitCommand(args) {
@@ -17,79 +17,131 @@ async function outputGitCommand(args) {
 }
 
 function parseGitStatus(output) {
-  return output.split('\n')
-    .filter(line => line)
-    .map(line => {
-      const rawStatus = line.substr(0, 2);
-      const filename = line.substr(3);
-      let stagingStatus = rawStatus[0] === ' ' || rawStatus[0] === '?' ? 'unstaged' : 'staged';
-      let gitStatus = null;
+	return output
+		.split("\n")
+		.filter((line) => line)
+		.map((line) => {
+			const rawStatus = line.substr(0, 2);
+			const filename = line.substr(3);
+			let stagingStatus =
+				rawStatus[0] === " " || rawStatus[0] === "?" ? "unstaged" : "staged";
+			let gitStatus = null;
 
-      function getStatus(index) {
-        switch (rawStatus[index]) {
-          case 'M': gitStatus = 'modified'; break;
-          case 'D': gitStatus = 'deleted'; break;
-          case 'A': gitStatus = 'added'; break;
-          case 'R': gitStatus = 'renamed'; break;
-          case 'C': gitStatus = 'copied'; break;
-          case '?': gitStatus = 'untracked'; break;
-          case '!': gitStatus = 'ignored'; break;
-          case 'U': gitStatus = 'conflict'; break; // Unmerged state
-          // Add more cases here if needed
-        }
-      }
+			function getStatus(index) {
+				switch (rawStatus[index]) {
+					case "M":
+						gitStatus = "modified";
+						break;
+					case "D":
+						gitStatus = "deleted";
+						break;
+					case "A":
+						gitStatus = "added";
+						break;
+					case "R":
+						gitStatus = "renamed";
+						break;
+					case "C":
+						gitStatus = "copied";
+						break;
+					case "?":
+						gitStatus = "untracked";
+						break;
+					case "!":
+						gitStatus = "ignored";
+						break;
+					case "U":
+						gitStatus = "conflict";
+						break;
+				}
+			}
 
-      if (rawStatus[0] === ' ' || rawStatus[0] === '?') {
-        getStatus(1);
-      } else {
-        getStatus(0);
-      }
+			if (rawStatus[0] === " " || rawStatus[0] === "?") {
+				getStatus(1);
+			} else {
+				getStatus(0);
+			}
 
-      // Apply color-coding
-      let displayStatus = `[${rawStatus}]`;
-      displayStatus = stagingStatus === 'staged' ? kleur.green(displayStatus) : kleur.red(displayStatus);
+			// Apply color-coding
+			let displayStatus = `[${rawStatus}]`;
+			displayStatus =
+				stagingStatus === "staged"
+					? kleur.green(displayStatus)
+					: kleur.red(displayStatus);
 
-      return {
-        filename,
-        displayStatus,
-        rawStatus,
-        status: stagingStatus,
-        gitStatus,
-        selected: stagingStatus == 'staged',
-      };
-    });
+			return {
+				filename,
+				displayStatus,
+				rawStatus,
+				status: stagingStatus,
+				gitStatus,
+				selected: stagingStatus == "staged",
+			};
+		});
 }
 
+const displayColor = (status) => {
+	if (status === "staged" || status === true) return kleur.green;
+	if (status === "renamed") return kleur.blue;
+	if (status === "copied") return kleur.blue;
+	if (status === "untracked") return kleur.red;
+	if (status === "ignored") return kleur.magenta;
+	if (status === "added") return kleur.red;
+	if (status === "deleted") return kleur.red;
+	if (status === "modified") return kleur.blue;
+	if (status === "conflict") return kleur.yellow;
+	else return kleur.red;
+};
+
 async function toggleFiles(files) {
-	let choices = files.map((file) => ({
-		name: ` ${file.displayStatus} ${file.filename} (${file.gitStatus || file.status})`,
-		value: file.filename,
-		checked: file.selected,
-	}));
+	let selected = 0;
+	let choices = files.map((file) => {
+		const color = displayColor(file.selected);
+		if (file.selected) selected++;
+		let gitStatus = file.gitStatus || file.status;
+		return {
+			title: `${color(file.displayStatus)} ${file.filename} (${displayColor(
+				gitStatus,
+			)(gitStatus)})`,
+			value: file.filename,
+			selected: file.selected,
+		};
+	});
 
-	const prompt = inquirer.createPromptModule();
-	const answers = await prompt([
-		{
-			type: "checkbox",
-			name: "filesToToggle",
-			message: "Select for staging:",
-			choices,
-			prefix: "🏗️ ",
-			loop: false,
+	const response = await prompts({
+		type: "multiselect",
+		name: "filesToToggle",
+		message: "Select for staging:",
+		choices,
+		onRender: function () {
+			if (selected !== this.selected().length) {
+				for (let i = 0; i < this.value.length; i++) {
+					const file = this.value[i];
+					const choice = files.find((f) => f.filename === file.value);
+					let displayStatus = `[${choice.rawStatus}]`;
+					let gitStatus = choice.gitStatus || choice.status;
+					this.value[i].title = `${displayColor(file.selected)(
+						displayStatus,
+					)} ${choice.filename} (${displayColor(gitStatus)(gitStatus)})`;
+				}
+				selected = this.selected().length;
+			}
 		},
-	]);
+		hint: "(A) to select or deselect all",
+		instructions: false,
+	});
 
+	if (!response.filesToToggle) {
+		throw "Aborted";
+	}
 	const output = files.map((file) => ({
 		...file,
-		selected: answers.filesToToggle.includes(file.filename),
+		selected: response.filesToToggle.includes(file.filename),
 	}));
-	// console.log(output);
 	return output;
 }
 
 async function main() {
-	// if process has argv we forward everything to git
-
 	try {
 		const isGit = await runGitCommand(["rev-parse", "--is-inside-work-tree"]);
 		if (isGit.trim() !== "true") {
